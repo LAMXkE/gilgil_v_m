c8fcc561a7e294bce2e4c16c0b04e2da0acdb4ec
LAMXkE <sametim17@gmail.com>
2019-07-24 19:47:57 +0900

    Initial add

---
+++ widget.cpp
@@ -0,0 +1,125 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    changeMoney(0);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::returnMoney(){
+    int cnt[4] = {0};
+    while(mon / 500 > 0 ){
+        cnt[0]++;
+        mon -= 500;
+    }
+    while(mon / 100 > 0){
+        cnt[1]++;
+        mon -= 100;
+    }
+    while(mon / 50 > 0){
+        cnt[2]++;
+         mon -= 50;
+    }
+    while(mon / 10 > 0 ){
+        cnt[3]++;
+        mon -= 10;
+    }
+
+    QMessageBox msg;
+    msg.information(nullptr,"Return", "Returned\n500 * " + QString::number(cnt[0]) + "\n"
+            +"100 * "+QString::number(cnt[1]) + "\n"
+            +"50 * "+QString::number(cnt[2]) + "\n"
+            +"10 * "+QString::number(cnt[3]) + "\n"
+            );
+    ui->money->display(QString::number(mon));
+    changeMoney(0);
+
+
+}
+
+
+void Widget::changeMoney(int n){
+    mon += n;
+    if(mon < 100){
+        ui->pbCoffee->setEnabled(false);
+        ui->pbTea->setEnabled(false);
+        ui->pbIcetea->setEnabled(false);
+    }else if(mon < 150){
+        ui->pbCoffee->setEnabled(true);
+        ui->pbTea->setEnabled(false);
+        ui->pbIcetea->setEnabled(false);
+    }else if(mon < 200){
+        ui->pbCoffee->setEnabled(true);
+        ui->pbTea->setEnabled(true);
+        ui->pbIcetea->setEnabled(false);
+    }else{
+        ui->pbCoffee->setEnabled(true);
+        ui->pbTea->setEnabled(true);
+        ui->pbIcetea->setEnabled(true);
+    }
+
+
+
+    if(n > 0){
+        QMessageBox msg;
+        msg.information(nullptr,"Money Inserted", "Inserted " + QString::number(n));
+        ui->money->display(QString::number(mon));
+    }else if(n<0){
+        QMessageBox msg;
+        msg.information(nullptr,"Payment Done", "Used " + QString::number(-1*n));
+        ui->money->display(QString::number(mon));
+    }
+
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+void Widget::on_pb100_clicked()
+{
+
+    changeMoney(100);
+}
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+
+void Widget::on_pbCoffee_clicked()
+{
+    changeMoney(-100);
+}
+
+void Widget::on_pbTea_clicked()
+{
+    changeMoney(-150);
+}
+
+void Widget::on_pbIcetea_clicked()
+{
+    changeMoney(-200);
+}
+
+void Widget::on_pbreturn_clicked()
+{
+    returnMoney();
+}
---
+++ widget.h
@@ -0,0 +1,43 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    void changeMoney(int n);
+    void returnMoney();
+
+private slots:
+    void on_pb10_clicked();
+
+    void on_pb50_clicked();
+
+    void on_pb100_clicked();
+
+    void on_pb500_clicked();
+
+    void on_pbCoffee_clicked();
+
+    void on_pbTea_clicked();
+
+    void on_pbIcetea_clicked();
+
+    void on_pbreturn_clicked();
+
+private:
+    Ui::Widget *ui;
+    int mon{0};
+};
+
+#endif // WIDGET_H
